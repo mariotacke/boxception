a58fdb7765047d93588dfbd4572ed9236dbeaa8a
Mario Tacke <tacke.mario@gmail.com>
2018-11-09 18:56:10 -0800

    feat: add square canvas drawing and tests

---
+++ test.js
@@ -0,0 +1,63 @@
+const assert = require('assert');
+const boxception = require('./lib/boxception');
+
+// this display helper provides a way for the assertion to show indented code
+const displayHelper = (expectedOutput) => expectedOutput.replace(/^ +/gm, '');
+
+describe('Boxception', () => {
+  it('should properly calculate a single box', () => {
+    assert.strictEqual(
+      boxception(1),
+      displayHelper(
+        `+-+
+         | |
+         +-+`
+      ),
+    );
+  });
+
+  it('should properly calculate nested boxes', () => {
+    assert.strictEqual(
+      boxception(2),
+      displayHelper(
+        `+---+
+         |+-+|
+         || ||
+         |+-+|
+         +---+`
+      ),
+    );
+  });
+
+  it('should properly pad nested boxes', () => {
+    assert.strictEqual(
+      boxception(2, 1),
+      displayHelper(
+        `+-----+
+         |     |
+         | +-+ |
+         | | | |
+         | +-+ |
+         |     |
+         +-----+`
+      ),
+    );
+  });
+
+  it('should properly pad nested boxes', () => {
+    assert.strictEqual(
+      boxception(2, 2),
+      displayHelper(
+        `+-------+
+         |       |
+         |       |
+         |  +-+  |
+         |  | |  |
+         |  +-+  |
+         |       |
+         |       |
+         +-------+`
+      ),
+    );
+  });
+});
